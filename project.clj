3c489acd87e3f5d975c96c5480317402ff021443
carlosesteves <carlosesteves@qa-metro-mac>
2014-05-24 22:24:03 +0100

    Created project skeleton

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject quality-analytics "0.1.0-SNAPSHOT"
+  :description "Quality analytics"
+  :url "https://github.com/carlosesteves/quality-analytics"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/analytics/core_test.clj
@@ -0,0 +1,6 @@
+(ns qa.core-test
+  (:require [clojure.test :refer :all]
+            [qa.core :refer :all]))
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
